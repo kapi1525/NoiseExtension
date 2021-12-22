1d0526fb538a08bed2dc92d924a215f9f1fae4c1
kapigames <kapigamesdev@gmail.com>
2021-12-22 06:00:46 +0100

    New fancy build script

--- build.bat
+++ build.bat
@@ -1,74 +1,147 @@
 @echo off
 
-SET runpath=%~dp0
-SET ctfpath="C:\Program Files (x86)\Steam\steamapps\common\Clickteam Fusion 2.5\"
-SET "arg=%1"
+:: ------------------------------
+::         build.bat v1.1        
+:: ------------------------------
+:: Run arguments:
+:: --fast       -f      build only windows
+:: --full       -F      build everything
+:: 
+:: --debug      -d
+:: --release    -r
+::
+:: --noclean    -c
+:: --noinstall  -i
+::
+:: --bundle     -b      create zip file with extension
+:: ------------------------------
 
-IF "%arg%"=="--help" goto :help
-IF "%arg%"=="/help" goto :help
-IF "%arg%"=="-h" goto :help
-IF "%arg%"=="/h" goto :help
-IF "%arg%"=="-?" goto :help
-IF "%arg%"=="/?" goto :help
+setlocal enabledelayedexpansion
 
-echo Building Noise...
-echo IF MSBUILD IS NOT FOUND TRY ADDING MSBUILD DIR TO THE PATH!
+:: Modify this path if you have fusion installed somewhere else.
+set ctf_path="C:\Program Files (x86)\Steam\steamapps\common\Clickteam Fusion 2.5\"
 
-rmdir MFX /s /q
+set project=Noise
 
-IF "%arg%"=="--debug" goto :debug
-IF "%arg%"=="/debug" goto :debug
-
-IF "%arg%"=="--fast" goto :fast
-IF "%arg%"=="/fast" goto :fast
-IF 1==1 goto :build
-
-
-
-:build
-msbuild -t:"Noise_Windows" -p:Configuration="Edittime Unicode" -p:Platform="x86" ./Noise.sln
-msbuild -t:"Noise_Windows" -p:Configuration="Runtime Unicode" -p:Platform="x86" ./Noise.sln
-msbuild -t:"Noise_Android" -p:Configuration="Runtime" -p:Platform="x86" ./Noise.sln
-msbuild -t:"Noise_Android" -p:Configuration="Runtime" -p:Platform="x64" ./Noise.sln
-msbuild -t:"Noise_Android" -p:Configuration="Runtime" -p:Platform="ARM" ./Noise.sln
-msbuild -t:"Noise_Android" -p:Configuration="Runtime" -p:Platform="ARM64" ./Noise.sln
-goto :finish
+:: Thiese are deafult
+set full=1
+set release=1
+set clean=1
+set install=1
+set bundle=0
 
 
-:debug
-msbuild -t:"Noise_Windows" -p:Configuration="Debug Unicode" -p:Platform="x86" ./Noise.sln
-msbuild -t:"Noise_Android" -p:Configuration="Debug" -p:Platform="x86" ./Noise.sln
-msbuild -t:"Noise_Android" -p:Configuration="Debug" -p:Platform="x64" ./Noise.sln
-msbuild -t:"Noise_Android" -p:Configuration="Debug" -p:Platform="ARM" ./Noise.sln
-msbuild -t:"Noise_Android" -p:Configuration="Debug" -p:Platform="ARM64" ./Noise.sln
-goto :finish
+set arg_count=0
+for %%x in (%*) do (
+    if %%~x==--fast (
+        set full=0
+    ) else if %%~x==-f (
+        set full=0
+    ) else if %%~x==--full (
+        set full=1
+    ) else if %%~x==-F (
+        set full=1
+    ) else if %%~x==--debug (
+        set release=0
+    ) else if %%~x==-d (
+        set release=0
+    ) else if %%~x==--release (
+        set release=1
+    ) else if %%~x==-r (
+        set release=1
+    ) else if %%~x==--noclean (
+        set clean=0
+    ) else if %%~x==-c (
+        set clean=0
+    ) else if %%~x==--noinstall (
+        set install=0
+    ) else if %%~x==-i (
+        set install=0
+    ) else if %%~x==--bundle (
+        set bundle=1
+    ) else if %%~x==-b (
+        set bundle=1
+    ) else (
+        echo Argument %%~x is not defined!
+        goto :exit
+    )
+)
 
 
-:fast
-msbuild -t:"Noise_Windows" -p:Configuration="Debug Unicode" -p:Platform="x86" ./Noise.sln
-goto :finish
+if %clean%==1 (
+    call :clean
+)
 
+if %release%==1 (
+    call :build "%project%_Windows" , "Edittime Unicode" , "x86"
+    call :build "%project%_Windows" , "Runtime Unicode" , "x86"
+) else (
+    call :build "%project%_Windows" , "Debug Unicode" , "x86"
+)
 
-:help
-echo Why? - This tool was made to make not using vs 2022 easier.
-echo Run arguments:
-echo   --debug /debug  -  Build debug
-echo   --fast /fast  -  Build only debug unicode windows.
-echo   --help -h -? /help /h /?  -  Display this very unhelpfull message.
-echo Common errors:
-echo   'msbuild' is not recognized as an internal or external command, operable program or batch file.  -  Find where msbuild is installed and add that path to PATH env variable.
-echo   Failed to copy and simmilar errors  -  Try to modify ctfpath on top of this bat file to path to your CTF instalation.
+if %full%==1 (
+    if %release%==1 (
+        call :build "%project%_Android" , "Runtime" , "ARM"
+        call :build "%project%_Android" , "Runtime" , "ARM64"
+        call :build "%project%_Android" , "Runtime" , "x86"
+        call :build "%project%_Android" , "Runtime" , "x64"
+    ) else (
+        call :build "%project%_Android" , "Debug" , "ARM"
+        call :build "%project%_Android" , "Debug" , "ARM64"
+        call :build "%project%_Android" , "Debug" , "x86"
+        call :build "%project%_Android" , "Debug" , "x64"
+    )
+)
+
+if %install%==1 (
+    call :install
+)
+
+if %bundle%==1 (
+    call :bundle
+)
+
 goto :exit
 
 
+:clean
+    call :run rmdir MFX /s /q
+    exit /B 0
 
-:finish
-echo Instaling extension...
-echo IF IT FAILS MODIFY CTFPATH OR CLOSE CTF!
 
-echo xcopy /s /v /c /y %runpath%\MFX\*.* %ctfpath%
-xcopy /s /v /c /y %runpath%\MFX\*.* %ctfpath%
+:build
+    :: arguments:
+    :: - project name
+    :: - configuration
+    :: - platform
+    call :run msbuild ./%project%.sln -t:"%~1" -p:Configuration="%~2" -p:Platform="%~3" -nologo -m -clp:Summary;ForceNoAlign;verbosity=minimal
+    exit /B 0
 
-echo Done.
+
+:install
+    TASKLIST | FINDSTR /I "mmf2u.exe" > NUL
+    IF NOT ERRORLEVEL 1 (
+        echo Waiting for fusion to close...
+        timeout /T 1 > NUL
+        GOTO :install
+    )
+
+    call :run xcopy /s /v /c /y %~dp0MFX\*.* %ctf_path%
+    exit /B 0
+
+
+:bundle
+    call :run tar -a -cf %project%.zip -C MFX *.*
+    exit /B 0
+
+
+:run
+    :: arguments:
+    :: - cmd
+    echo [CMD] %*
+    %*
+    exit /B 0
+
 
 :exit
+echo Hopefully %project% will work?
