ecf57b8e9071d2032e6530d7eccca8c30a456f40
kapigames <kapigamesdev@gmail.com>
2022-05-28 12:32:00 +0200

    Version bump

--- Noise/Extension.h
+++ Noise/Extension.h
@@ -20,7 +20,7 @@ public:
 	Edif::Runtime Runtime;
 
 	static const int MinimumBuild = 254;
-	static const int Version = 11;
+	static const int Version = 12;
 
 	static const OEFLAGS OEFLAGS = OEFLAGS::NONE;
 	static const OEPREFS OEPREFS = OEPREFS::NONE;
