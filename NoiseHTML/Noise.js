b8c0915c58bb95ec2e3cf996120d6bde0637343a
kapi1525 <kacperbugla@proton.me>
2024-02-11 20:27:29 +0100

    Bump version to 23

--- NoiseHTML/Noise.js
+++ NoiseHTML/Noise.js
@@ -26,7 +26,7 @@ export default class CRunNoise extends CRunExtension {
 
         // DarkEdif SDK exts should have these four variables defined.
         // We need this[] and globalThis[] instead of direct because HTML5 Final Project minifies and breaks the names otherwise
-        this['ExtensionVersion'] = 22;      // To match C++ version
+        this['ExtensionVersion'] = 23;      // To match C++ version
         this['SDKVersion'] = 18;            // To match C++ version
         this['DebugMode'] = true;
         this['ExtensionName'] = 'Noise';
